2ddbdaa67a1bfe5bdb1d5ef9d0cedfd3d286d752
killshotxd <hassanansari211@gmail.com>
2023-03-28 11:31:38 +0530

    Update SignIn.jsx

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SignIn = () => {
   return (
-    <section>
+    <section className="containerWrap" style={{ marginTop: "8rem" }}>
       <div className="grid grid-cols-1 lg:grid-cols-2">
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 lg:px-8 sm:py-16 lg:py-24">
           <div className="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
