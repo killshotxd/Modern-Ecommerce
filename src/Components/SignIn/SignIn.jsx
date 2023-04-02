b59f7cf15927d62c555a048a2af9a0da4ed96262
killshotxd <hassanansari211@gmail.com>
2023-04-02 17:52:15 +0530

    Update SignIn.jsx

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -20,62 +20,25 @@ export const SignIn = () => {
   }, [currentUser]);
 
   return (
-    <section className="containerWrap" style={{ marginTop: "8rem" }}>
-      <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div className="flex items-center justify-center px-4 py-10 sm:px-6 lg:px-8 sm:py-16 lg:py-24">
-          <div className="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
-            <h2 className="text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl">
-              Modern E-commerce
-            </h2>
-            {/* <p className="mt-2 text-base text-gray-600 dark:text-gray-300">
-              Don&apos;t have an account?{" "}
-              <a
-                href="#"
-                title=""
-                className="font-medium text-indigo-600 transition-all duration-200 hover:text-indigo-700 hover:underline focus:text-indigo-700"
-              >
-                Create a free account
-              </a>
-            </p> */}
-            <p className="mt-2 text-base text-gray-600 dark:text-gray-300">
-              Your one-stop shop - sign in to start.
-            </p>
-            <p className="mt-2 text-base text-gray-600 dark:text-gray-300">
-              Revolutionize your shopping experience with our modern e-commerce
-              web app. Discover, shop, and save with ease.
-            </p>
-
-            <div className="mt-3 space-y-3">
-              <button
-                onClick={handleLogin}
-                type="button"
-                className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 dark:text-gray-400 transition-all duration-200 bg-white border border-gray-500 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
-              >
-                <div className="absolute inset-y-0 left-0 p-4">
-                  <svg
-                    className="w-6 h-6 text-rose-500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                  >
-                    <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                  </svg>
-                </div>
-                Sign in with Google
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-full h-full">
-          <img
-            className="w-full h-full mx-auto object-cover"
-            src="https://cdn.shopify.com/shopifycloud/brochure/assets/landers/ppc/ecommerce/hero-small-accdfa5d4628cee00ae2a5c2694e164c1eec8b247143f59ad27dfdb232c8d819.png"
-            alt=""
-          />
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url("https://images.unsplash.com/photo-1455849318743-b2233052fcff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80")`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-lg">
+          <h1 className="mb-5 text-5xl font-bold">Modern E-Commerce</h1>
+          <p className="mb-5">
+            We Don't Make Ecommerce. We Make Ecommerce Better.
+          </p>
+          <button onClick={handleLogin} className="btn glass">
+            Get Started
+          </button>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
